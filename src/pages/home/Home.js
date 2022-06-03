81970ab1f43701ddcdfc0a26bdbb83b7b52a9332
IanHancock36 <northroadian@gmail.com>
2022-06-03 15:10:55 -0500

    change

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,15 +3,17 @@ import RecipeList from "../../components/RecipeList";
 import { useFetch } from "../../hooks/useFetch";
 import "./Home.css";
 import "./components/RecipeList"
+
 function Home() {
   const { data, isPending, error } = useFetch("http://localhost:8000/recipes");
   return (
     <div className="home">
       {error && <p>{error}</p>}
       {isPending && <p className="loading">Loading...</p>}
-{data && RecipeList/>}
+{data && <RecipeList/>}
     </div>
   );
 }
 
 export default Home;
+
